713218baf9f0f10a800be6501a83dbb347cb7abe
paul.huwiler <paul.huwiler@edu.hefr.ch>
2025-01-04 12:14:50 +0100

    add Hasher

---
+++ hasher.py
@@ -0,0 +1,15 @@
+import hashlib
+
+def sha512Hash(data: str) -> str:
+    encoded_data = data.encode('utf-8')
+    hash_object = hashlib.sha512(encoded_data)
+    hex_digest = hash_object.hexdigest()
+    return hex_digest
+
+def writInFile(data : str):
+    #TODO
+
+if __name__ == "__main__":
+    texte = input("Entrez le txt : ")
+    print("Texte à hasher :", texte)
+    print("SHA-512       :", sha512Hash(texte))
